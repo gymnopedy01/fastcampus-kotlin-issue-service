cb3c88301b48947b03cf75f386d3a116a19fba50
Taehyun Oh <gymnopedy01@gmail.com>
2022-11-01 12:30:32 +0900

    7-Ch01 회원인증서비스 개발하기  - 공통 에러처리  - 토큰 기반 인증

---
+++ user-service/src/main/kotlin/com/fastcampus/userservice/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,37 @@
+package com.fastcampus.userservice.exception
+
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.mono
+import mu.KotlinLogging
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Configuration
+class GlobalExceptionHandler(private val objectMapper: ObjectMapper) : ErrorWebExceptionHandler {
+
+    private val logger = KotlinLogging.logger {}
+    override fun handle(exchange: ServerWebExchange, ex: Throwable): Mono<Void> = mono {
+
+        logger.error { ex.message }
+
+        val errorResponse = if (ex is ServerException) {
+            ErrorResponse(code = ex.code, message = ex.message)
+        } else {
+            ErrorResponse(code = 500, message = "Internal Server Error")
+        }
+
+        with(exchange.response) {
+            statusCode = HttpStatus.OK
+            headers.contentType = MediaType.APPLICATION_JSON
+
+            //응답객체를 제이슨으로 쓰려고 하려면 버퍼팩토리라는 기능을 통해서 사용 해야 하는데요.
+            val dataBuffer = bufferFactory().wrap(objectMapper.writeValueAsBytes(errorResponse))
+            writeWith(dataBuffer.toMono()).awaitSingle()
+        }
+
+    }
+}
