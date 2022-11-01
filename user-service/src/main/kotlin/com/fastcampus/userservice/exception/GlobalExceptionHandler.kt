b4a78ba3e1bae926a4c7fbb97940c19a8237c4fe
Taehyun Oh <gymnopedy01@gmail.com>
2022-11-01 14:59:52 +0900

    7-Ch01 회원인증서비스 개발하기  - 공통 에러처리  - 토큰 기반 인증

--- user-service/src/main/kotlin/com/fastcampus/userservice/exception/GlobalExceptionHandler.kt
+++ user-service/src/main/kotlin/com/fastcampus/userservice/exception/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.fastcampus.userservice.exception
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.reactor.mono
 import mu.KotlinLogging
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 
 @Configuration
 class GlobalExceptionHandler(private val objectMapper: ObjectMapper) : ErrorWebExceptionHandler {
