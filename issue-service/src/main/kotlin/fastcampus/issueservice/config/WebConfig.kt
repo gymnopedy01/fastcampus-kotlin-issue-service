c21b73833b2e29938b8b15aaba9f9dcf2413752d
오태현(Oh Taehyun)(fuga) <fuga@ncsoft.com>
2022-11-07 14:17:17 +0900

    7- 이슈 관리 프로젝트로 코틀린 & 스프링 WebFlux 완벽학습 Ch02 프로젝트 마무리 - 이슈 관리 서비스와 회원 인증 서비스 연동하기 - 프론트 연동 및 기능확인

--- issue-service/src/main/kotlin/fastcampus/issueservice/config/WebConfig.kt
+++ issue-service/src/main/kotlin/fastcampus/issueservice/config/WebConfig.kt
@@ -1,5 +1,9 @@
 package fastcampus.issueservice.config
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import fastcampus.issueservice.exception.UnAuthorizedException
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
@@ -7,6 +11,9 @@ import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 
 @Configuration
@@ -22,6 +29,17 @@ class WebConfig(
             add(authUserHandlerMethodArgumentResolver)
         }
     }
+
+    //정적 리소스 접근하려면 AddResourceHandlers 설정을 해줘야함
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations(*arrayOf(
+                "classpath:/META-INF/resources",
+                "classpath:/resources/",
+                "classpath:/static/",
+                "classpath:/public/",
+            ))
+    }
 }
 
 
@@ -29,7 +47,9 @@ class WebConfig(
 // 서포츠 파라미터에 해당하는 파라미터에 해당 조건이 맞는다면
 // 리졸브 아규먼트를 통해서 해당 어노테이션 또는 객체에대해서 세팅 또는 조작을 할수있는 인터페이스
 @Component
-class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
+class AuthUserHandlerArgumentResolver(
+    @Value("\${auth.url}") val authUrl: String,
+    ) : HandlerMethodArgumentResolver {
 
     //파라미터로 들어온 AuthUser 와 AuthUserClass타입이 동일하다면 이조건은 만족하여
     //하위에 있는 resolveArgument가 동작함
@@ -43,16 +63,32 @@ class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
         binderFactory: WebDataBinderFactory?
     ): Any? {
         //더미 유저 객체
-        return AuthUser(
-            userId = 1,
-            username = " 테스트 ",
-        )
+//        return AuthUser(
+//            userId = 1,
+//            username = " 테스트 ",
+//        )
+
+        val authHeader = webRequest.getHeader("Authorization") ?: throw UnAuthorizedException()
+
+        // RestTemplate 대신 WebClient를 사용
+        return runBlocking {
+            WebClient.create()
+                .get()
+                .uri(authUrl)
+                .header("Authorization", authHeader)        //Bearer eXydjxkck.sdf.asdf.
+                .retrieve()
+                .awaitBody<AuthUser>()      // bodytoMono 사용해도되지만 코루틴 기반의 awaitBody 확장함수 도 제공
+                                            //일반 함수이기때문에 awaitBody 시 suspend가 필요, runBlocking 으로 감싼다.
+        }
+
     }
 
 }
 
 data class AuthUser(
+    @JsonProperty("id")
     val userId: Long,
     val username: String,
+    val email:String,
     val profileUrl: String? = null
 )
